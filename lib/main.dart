c7576f004ff7784a69bf8699bd2180366657a3b6
poonam kumawat <mailpoonam2002@gmail.com>
2022-11-08 22:32:52 +0530

    Created OnBoarding Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:ondoorstep/views/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -10,21 +12,16 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Firebase'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const OnboardingScreen(),
     );
   }
 }
