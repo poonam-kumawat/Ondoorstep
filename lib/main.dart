85e59179ba88623e1196c7e2ecaad6088c4b3850
poonam kumawat <mailpoonam2002@gmail.com>
2022-11-09 21:27:22 +0530

    Added Login Design

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
+import 'package:ondoorstep/Login/login.dart';
+import 'package:ondoorstep/Login/otp.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -10,21 +13,18 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: 'phone',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Firebase'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      routes: {
+        'phone': (context) => MyPhone(),
+        'verify': (context) => MyVerify()
+      },
     );
   }
 }
